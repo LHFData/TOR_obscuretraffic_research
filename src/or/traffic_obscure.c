b3f2751a86a194a50292dd8b8013edbf63271da7
Liohoifung <liaohaifeng@bupt.edu.cn>
2018-05-02 14:58:22 +0800

    tor-3.1.7 version source code

---
+++ src/or/traffic_obscure.c
@@ -0,0 +1,4 @@
+include "connection.h"
+static int obscure_on_conn(const connection_t *conn){
+    if(conn->)
+}
---
+++ src/or/traffic_obscure.h
@@ -0,0 +1,11 @@
+#ifndef TOR_OBSCURE_H
+#define TOR_OBSCURE_H
+typedef enum{
+    obscure_none,
+    obscure_later,
+    obscure_scheduled,
+    obscure_already_scheduled,
+    obscure_sent,
+} obscure_descision_t;
+
+#endif
