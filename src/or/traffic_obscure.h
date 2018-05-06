be3852c02b546441cad83afb1c41c1e92ba8af4a
Liohoifung <liaohaifeng@bupt.edu.cn>
2018-05-06 14:36:36 +0800

    "header file change"

--- src/or/traffic_obscure.h
+++ src/or/traffic_obscure.h
@@ -1,5 +1,8 @@
 #ifndef TOR_OBSCURE_H
 #define TOR_OBSCURE_H
+
+#include <stdint.h>
+#include "trunnel.h"
 typedef enum{
     obscure_none,
     obscure_later,
@@ -8,4 +11,5 @@ typedef enum{
     obscure_sent,
 } obscure_descision_t;
 
+void obscure_disable_on_channel(channel_t* chan);
 #endif
